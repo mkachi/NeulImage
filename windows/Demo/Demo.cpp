6e94011fc81151d888846e71e05697fe4e21b562
맛카치 <mkachi@naver.com>
2018-01-19 20:17:55 +0900

    Add Demo project code

---
+++ windows/Demo/Demo.cpp
@@ -0,0 +1,21 @@
+#include "Demo.h"
+
+void Init()
+{
+
+}
+
+void Update(float dt)
+{
+
+}
+
+void Render()
+{
+
+}
+
+void Destroy()
+{
+
+}
---
+++ windows/Demo/Demo.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void Init();
+void Update(float dt);
+void Render();
+void Destroy();
